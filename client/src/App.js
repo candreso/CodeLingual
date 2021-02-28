10eeb845f4a6e6a7c455c2802cfece1ce0f8e17d
Xapollon430 <Xapollon430@gmail.com>
2021-02-28 18:33:08 -0500

    bruh

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import Header from "./components/Header";
+import Header from "./components/Header/Header";
 import Translate from "./components/Translate/Translate";
 function App() {
   return (
--- client/src/components/Header.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Header.css";
-
-const Header = () => {
-  return <div className="red">123</div>;
-};
-
-export default Header;
