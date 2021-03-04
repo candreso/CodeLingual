38164871079d44cfa6d5436abd0db747412f0023
candreso <candreso@gmu.edu>
2021-03-04 17:34:46 -0500

    finished routes.js first draft

--- server/api/routes.js
+++ server/api/routes.js
@@ -1,21 +1,23 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const trnsCtrl = require("./translate.controller");
+const admnCtrl = require("./admin.controller");
 
+const router = express.Router();
 const translatePath = "/translate";
 const adminPath = "/admin";
 
 /* MAIN PAGES */
-router.route("/").get();
+router.route("/").get(trnsCtrl.translate);
 // other pages with no associated requests: /about
 
 /* TRANSLATE PAGES */
-router.route(`${translatePath}/`).get();
-router.route(`${translatePath}/add/`).post();
-router.route(`${translatePath}/report/`).put();
+router.route(`${translatePath}/`).get(trnsCtrl.translate);
+router.route(`${translatePath}/add/`).post(trnsCtrl.addTranslation);
+router.route(`${translatePath}/report/`).put(trnsCtrl.reportTranslation);
 
 /* ADMIN PAGES */
-router.route(`${adminPath}/login/`).get(); // change to POST for security reasons?
-router.route(`${adminPath}/dashboard/`);
-// router.route(`${adminPath}/register/`).post(); // questionable
+router.route(`${adminPath}/login/`).get(admnCtrl.login); // change to POST for security reasons?
+router.route(`${adminPath}/dashboard/`).post(adminCtrl.processChanges);
+// router.route(`${adminPath}/register/`).post(); // not set-in-stone
 
-export default router;
+module.exports = router;
