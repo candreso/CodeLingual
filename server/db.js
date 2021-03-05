018e67f3b792f39cf00632486baefa67882adcec
Xapollon430 <Xapollon430@gmail.com>
2021-03-04 21:49:20 -0500

    123

--- server/db.js
+++ server/db.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 const env = require("dotenv");
 env.config();
 
-// mongoose.connect(
-//   process.env.MONGO_URL,
-//   {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   },
-//   () => {
-//     // We need a db link from the .env file to connect to MongoDB.
-//     console.log("Connected to database!");
-//   }
-// );
+mongoose.connect(
+  process.env.MONGO_URL,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    // We need a db link from the .env file to connect to MongoDB.
+    console.log("Connected to database!");
+  }
+);
