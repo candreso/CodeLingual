6f25cba6fa09ededc206cd722f33e80d1abb6e7f
candreso <candreso@gmu.edu>
2021-03-04 19:31:58 -0500

    added framework for translate controller and code in server.js to test

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const routes = require("./api/routes");
 
 require("dotenv").config();
 require("./db.js");
@@ -7,7 +8,10 @@ require("./db.js");
 const app = express();
 const port = process.env.PORT || 5000; // Checking if env port is given. If not default to 5000.
 
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+/* Register API routes */
+app.use("/", routes);
+app.use("/", express.static(path.resolve(__dirname, "../client/build")));
+// app.use("*", (req, res) => res.status(404).json({ error: "not found" }));
 
 /* app.get("/", (req, res) => {
   res.send("<h1>Hey peeps</h1>");
