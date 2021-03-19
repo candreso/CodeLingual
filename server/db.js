36f1095be74739a50b0ee462ca0c8d33b739fa5c
candreso <candreso@gmu.edu>
2021-03-19 11:08:18 -0400

    added simple catch statement to db connection method to handle errors

--- server/db.js
+++ server/db.js
@@ -4,7 +4,12 @@ require("dotenv").config();
 MongoClient.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-}).then(() => {
-  // We need a db link from the .env file to connect to MongoDB.
-  console.log("Connected to database!");
-});
+})
+  .catch((err) => {
+    console.error(err.stack);
+    process.exit(1);
+  })
+  .then(() => {
+    // We need a db link from the .env file to connect to MongoDB.
+    console.log("Connected to database!");
+  });
