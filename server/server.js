95efe8beb40125989f3ae858d1dd4680a95e6f1a
candreso <candreso@gmu.edu>
2021-04-11 15:00:19 -0400

    restored server.js version from candreso/CodeLingual and registered * route to react pages

--- server/server.js
+++ server/server.js
@@ -2,23 +2,22 @@ const express = require("express");
 var cors = require("cors");
 const path = require("path");
 const routes = require("./api/routes");
-const cors = require("cors");
+
 require("dotenv").config();
-// require("./db.js");
+require("./db.js");
 
 const app = express();
 const port = process.env.PORT || 5000; // Checking if env port is given. If not default to 5000.
 
-app.use(express.json());
+/* Enable CORS so client running in development mode (on port 3000) can access resources from
+ * server. Will most likely be unnecessary during more advanced development stages. */
 app.use(cors());
-app.use(express.static(path.resolve(__dirname, "../client/build")));
+app.use(express.json());
+
 /* Register API routes */
 app.use("/api/v1", routes);
-app.get("/*", express.static(path.resolve(__dirname, "../client/build")));
-
-/* app.get("/", (req, res) => {
-  res.send("<h1>Hey peeps</h1>");
-}); */
+app.use("/", express.static(path.resolve(__dirname, "../client/build")));
+app.use("*", express.static(path.resolve(__dirname, "../client/build")));
 
 app.listen(port, () => {
   console.log(`Server started at http://localhost:${port}!`);
