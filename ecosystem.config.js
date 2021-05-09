216bab508f02468994154bca132af5963b51608a
candreso <candreso@gmu.edu>
2021-05-09 16:11:08 -0400

    changed pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,12 @@
 module.exports = {
   apps: [{
     script: './server/server.js',
-    watch: './server'
-  }],
-
-  deploy: {
-    production: {
-      'pre-deploy-local': '',
-      'post-deploy': 'pm2 reload ecosystem.config.js --env production',
-      'pre-setup': ''
+    watch: './server',
+    env: {
+      NODE_ENV: "development",
+    },
+    env_production: {
+      NODE_ENV: "production",
     }
-  }
+  }]
 };
